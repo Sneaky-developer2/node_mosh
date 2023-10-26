06ee0569024b3a720582c6b9be8191e069bbb5a6
sneaky_developer <noob123noob123jjxd@gmail.com>
2023-10-26 12:50:48 -0400

    nothing

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+
+
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
@@ -22,7 +24,7 @@ app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body)
   if (error) return res.status(400).send(result.error.details[0].message)
 
-  const course = { 
+  const course = {
     id: courses.length + 1,
     name: req.body.name,
   }
