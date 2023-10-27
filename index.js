803e689533b7bd954b1e537b924a0fe71b2cd420
sneaky_developer <noob123noob123jjxd@gmail.com>
2023-10-27 05:05:18 -0400

    env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const morgan = require("morgan")
+const helmet = require("helmet")
 const Joi = require("joi")
 const logger = require("./logger")
 const auth = require("./Auther")
@@ -7,6 +9,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
+app.use(helmet())
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"))
+  console.log("Morgan enabled...")
+}
 
 app.use(logger)
 app.use(auth)
